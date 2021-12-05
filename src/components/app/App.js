5570ebd0fc15f7505b355737cad2ffd89a3bda49
ilya <lawrence.il.pr@gmail.com>
2021-12-05 10:15:57 +0500

    changed paths to imgs

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,24 +4,18 @@ import Promo from '../../Promo/Promo';
 import aboutUs from '../../aboutUs/AboutUs';
 import OurBest from '../../OurBest/OurBest';
 import Footer from '../../Footer/Footer';
-import '../../media.sass';
+import '../../media-index.sass';
 
-import solimoCoffeeBeansX1 from '../../OurBest/img/SolimoCoffeeBeansX1.png';
-import solimoCoffeeBeansX2 from '../../OurBest/img/SolimoCoffeeBeansX2.png';
-import prestoCoffeeBeansX1 from '../../OurBest/img/PrestoCoffeeBeansX1.png';
-import prestoCoffeeBeansX2 from '../../OurBest/img/PrestoCoffeeBeansX2.png';
-import AROMISTICOCoffeeX1 from '../../OurBest/img/AROMISTICOCoffeeX1.png';
-import AROMISTICOCoffeeX2 from '../../OurBest/img/AROMISTICOCoffeeX2.png';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       itemsOurBest: [
-        {name: 'Solimo Coffee Beans', urlImgX1: solimoCoffeeBeansX1, urlImgX2: solimoCoffeeBeansX2, cost: 10.73, id: 1},
-        {name: 'Presto Coffee Beans', urlImgX1: prestoCoffeeBeansX1, urlImgX2: prestoCoffeeBeansX2, cost: 15.99, id: 2},
-        {name: 'AROMISTICO Coffee', urlImgX1: AROMISTICOCoffeeX1, urlImgX2: AROMISTICOCoffeeX2, cost: 6.99, id: 3},
-        {name: 'AROMISTICO Coffee', urlImgX1: AROMISTICOCoffeeX1, urlImgX2: AROMISTICOCoffeeX2, cost: 6.99, id: 4},
+        {name: 'Solimo Coffee Beans', urlImgX1: "./img/solimoCoffeeBeansX1.png", urlImgX2: './img/solimoCoffeeBeansX2.png', cost: 10.73, id: 1},
+        {name: 'Presto Coffee Beans', urlImgX1: "./img/prestoCoffeeBeansX1.png", urlImgX2: './img/prestoCoffeeBeansX2.png', cost: 15.99, id: 2},
+        {name: 'AROMISTICO Coffee', urlImgX1: './img/AROMISTICOCoffeeX1.png', urlImgX2: './img/AROMISTICOCoffeeX2.png', cost: 6.99, id: 3},
+        {name: 'AROMISTICO Coffee', urlImgX1: './img/AROMISTICOCoffeeX1.png', urlImgX2: './img/AROMISTICOCoffeeX2.png', cost: 6.99, id: 4},
       ],
     }
   }
